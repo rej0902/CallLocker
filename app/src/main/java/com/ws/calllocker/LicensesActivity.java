39839272368ffb9cf046827555dfbb0f5c6e6af0
rej0902 <rej0902@naver.com>
2017-05-09 06:19:50 +0900

    Licenses Activity 추가

---
+++ app/src/main/java/com/ws/calllocker/LicensesActivity.java
@@ -0,0 +1,24 @@
+package com.ws.calllocker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ws on 2017-05-09.
+ */
+
+public class LicensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.licenses);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
